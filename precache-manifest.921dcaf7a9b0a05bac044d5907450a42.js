b9ae53fce840a8cd1a3ad1dfa2d0d792b5243c11
ni774 <gnikhil556@gmail.com>
2022-02-04 21:25:14 +0530

    Updates

---
+++ precache-manifest.921dcaf7a9b0a05bac044d5907450a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "601ce727765f627be573129637b2a9d4",
+    "url": "/static/media/nikhil.601ce727.jpeg"
+  },
+  {
+    "revision": "8e55313caf5edc919fc30d231bd27e40",
+    "url": "/static/media/background.8e55313c.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d01b1b5c44b450d8bf8d",
+    "url": "/static/js/main.d01b1b5c.chunk.js"
+  },
+  {
+    "revision": "bc080222cf234f5f3b0a",
+    "url": "/static/js/1.bc080222.chunk.js"
+  },
+  {
+    "revision": "d01b1b5c44b450d8bf8d",
+    "url": "/static/css/main.c31d9793.chunk.css"
+  },
+  {
+    "revision": "ebdd6aa36eae16f15b8a5a4700703754",
+    "url": "/index.html"
+  }
+];
